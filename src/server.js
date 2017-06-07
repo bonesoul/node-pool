8abaeee2af73a59fec5a66000dad76a573ed5c3d
Huseyin Uslu <shalafiraistlin@gmail.com>
2017-06-07 17:33:34 +0300

    :wrench: initial commit.

---
+++ src/server.js
@@ -0,0 +1,11 @@
+//
+//     hpool
+//     Copyright (C) 2013 - 2017, Hüseyin Uslu, Int6ware - http://www.int6ware.com
+//
+'use strict';
+
+require('app-module-path').addPath(__dirname);
+const config = require('config');
+const os = require('os');
+const path = require('path');
+const winston = require('winston');
