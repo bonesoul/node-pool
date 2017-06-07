2a616e26035770ef0e44518d3a5cbe6a21f10b66
Huseyin Uslu <shalafiraistlin@gmail.com>
2017-06-07 17:55:33 +0300

    :wrench: initial startup work. :wrench: added gitignore.

---
+++ src/app.js
@@ -0,0 +1,33 @@
+//
+//     hpool
+//     Copyright (C) 2013 - 2017, Hüseyin Uslu, Int6ware - http://www.int6ware.com
+//
+'use strict';
+
+require('app-module-path').addPath(__dirname);
+const config = require('config');
+const os = require('os');
+const path = require('path');
+const winston = require('winston');
+const packageInfo = require('../package.json');
+
+async function startup () {
+  try {
+    // ========================================
+    // initialize env manager.
+    // ========================================
+    require('common/env.js');
+
+    // ========================================
+    // print startup banner.
+    // ========================================
+    winston.info('[APP] starting up hpool-server version: %s [%s]', packageInfo.version, env); // eslint-disable-line no-undef
+    winston.info('[APP] running on: %s-%s [%s %s]', os.platform(), os.arch(), os.type(), os.release());
+    winston.info('[APP] node: %s, v8: %s, uv: %s, openssl: %s', process.versions.node, process.versions.v8, process.versions.uv, process.versions.openssl);
+    winston.info('[APP] running over %d core system', os.cpus().length);
+  } catch (err) {
+    winston.error('startup error: %s', err);
+  }
+}
+
+startup();
---
+++ src/common/env.js
@@ -0,0 +1,16 @@
+//
+//     hpool
+//     Copyright (C) 2013 - 2017, Hüseyin Uslu, Int6ware - http://www.int6ware.com
+//
+'use strict';
+
+const prettyError = require('pretty-error');
+
+var EnvManager = (function () {
+  // check if we have an invalid node_env set.
+  if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') process.env.NODE_ENV = 'development'; // set to development by default.
+  var env = global.env = process.env.NODE_ENV;
+  if (env === 'development') prettyError.start(); // on development mode start the pretty errors.
+})();
+
+module.exports = exports = EnvManager;
