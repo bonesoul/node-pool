629135f1d33a837af3020c0df35c99e578989349
Huseyin Uslu <shalafiraistlin@gmail.com>
2017-06-14 15:03:40 +0300

    :sparkles: adding mocha & eslint support.

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+//
+//     hypeengine
+//     Copyright (C) 2013 - 2017, Hüseyin Uslu, Int6ware - http://www.int6ware.com
+//
+'use strict';
+
+module.exports = function (grunt) {
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    eslint: {
+      target: ['src/**/*.js', 'assets/js/**/*.js', 'test/**/*.js', 'contrib/**/*.js'],
+      options: {
+        configFile: '.eslintrc.yaml',
+        ingorePath: '.eslintignore',
+        format: 'html',
+        outputFile: 'build/lint/eslint/result.html',
+        maxWarnings: -1
+      }
+    }
+  });
+
+  // load all plugins.
+  require('load-grunt-tasks')(grunt);
+
+  // tasks.
+  //grunt.registerTask('test', ['mochaTest']);
+  grunt.registerTask('lint', ['eslint']);
+  grunt.registerTask('default', ['lint']);
+};
---
+++ test/bootstrap.js
@@ -0,0 +1,7 @@
+//
+//     hypeengine
+//     Copyright (C) 2013 - 2017, Hüseyin Uslu, Int6ware - http://www.int6ware.com
+//
+'use strict';
+
+process.env.NODE_ENV = 'test'; // change the env immediatly so everything runs in 'test' mode.
